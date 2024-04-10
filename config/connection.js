5d0a199a63402259071f8c9c383ce77d1d9db387
yoboyyash <yashrajbaviskar@gmail.com>
2024-04-10 15:06:38 -0500

    Heroku Test #6

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 const Sequelize = require('sequelize');
-require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    });
+require('dotenv').config();
+
+
+
+let sequelize;
+
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        },
+    );
+};
 
 module.exports = sequelize;
